23a36c6b5230b933cecbedc2290980c0f3fe0b3e
sona-user-369 <donadavak@gmail.com>
2024-04-10 10:29:54 +0100

    update for host 27b

--- SorayiaAPI/settings.py
+++ SorayiaAPI/settings.py
@@ -30,7 +30,7 @@ DEBUG = "RENDER" in os.environ
 # DEBUG = True
 
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', decouple.config('OPENAI_API_KEY'))
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
